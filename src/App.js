89b6041ab95c410334dfe304d600d769e909d356
vitoorfraga <vitorfragaps@gmail.comgit statusgit config --global user.email vitorfragaps@gmail.com>
2021-09-19 15:22:56 -0300

    Conversor de Moedas

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Conversor from './components/Conversor'
+
+function App() {
+  return (
+    <div className="App">
+      <Conversor moedaA="USD" moedaB="BRL"></Conversor> 
+      <Conversor moedaA="BRL" moedaB="USD"></Conversor> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Conversor.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import'./Conversor.css'
+
+export default class Conversor extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      moedaA_valor: "",
+      moedaB_valor: 0,
+    }
+    this.converter = this.converter.bind(this);
+  }
+
+  converter(){
+    let de_para = `${this.props.moedaA}_${this.props.moedaB}`
+    let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=cbe998448b8c1346e1a7`
+
+    fetch(url).then(res=>{
+      return res.json()
+    })
+    .then(json=>{
+      let cotacao = json[de_para];
+      let moedaB_valor = (parseFloat (this.state.moedaA_valor)* cotacao).toFixed(2) 
+      this.setState({moedaB_valor})
+    })
+  }
+
+  render() {
+    return (
+      <div className="conversor">
+        <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+        <input type='text' onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+        <button type='button' value='converter' onClick={this.converter}>Converter</button>
+        <h2>Valor convertido</h2>
+        <h2>{this.state.moedaB_valor}</h2>
+      </div>
+    )
+  }
+}
